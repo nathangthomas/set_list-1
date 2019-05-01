e69aefce04b4f76ab33b00ca5d5a7d378df2bb55
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-05-01 11:48:14 -0600

    models migrations and dbs lesson

---
+++ spec/models/song_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe Song, type: :model do
+  describe "relationship" do
+    it {should belong_to :artist}
+  end
+
+  describe 'class methods' do
+    it '.song_count' do
+      prince = Artist.create!(name: 'Prince')
+      talking_heads = Artist.create!(name: 'Talking Heads')
+      raspberry_beret = prince.songs.create(title: 'Raspberry Beret')
+      wild_life = talking_heads.songs.create(title: 'Wild Wild Life')
+
+      expect(Song.song_count).to eq(2)
+    end
+  end
+end
