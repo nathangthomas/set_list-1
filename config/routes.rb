7b830a55f58772e5a5cf1c51b2e9288b6d7ea012
Brian Zanti <brianzanti@gmail.com>
2019-05-08 12:28:36 -0600

    nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/songs', to: 'songs#index'
-  get '/songs/:id', to: 'songs#show'
+  resources :songs, only: [:index, :show]
 
-  get '/artists/new', to: 'artists#new'
-  post '/artists', to: 'artists#create'
-  get '/artists/:id', to: 'artists#show'
+  # get '/songs', to: 'songs#index'
+  # get '/songs/:id', to: 'songs#show'
 
-  get '/playlists', to: "playlists#index"
+  resources :artists, only: [:new, :create, :show] do
+    resources :songs, only: [:new, :create]
+  end
+
+  # get '/artists/new', to: 'artists#new'
+  # post '/artists', to: 'artists#create'
+  # get '/artists/:id', to: 'artists#show'
+
+  resources :playlists, only: :index
+
+  # get '/playlists', to: "playlists#index"
 end
---
+++ spec/features/songs/new_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "creating a new song" do
+  it "can create a song" do
+    artist = Artist.create!(name: "Journey")
+    title = "Don't Stop Believin"
+    length = 79584
+    play_count = 647839
+
+    visit new_artist_song_path(artist)
+    save_and_open_page
+    fill_in :song_title, with: title
+    fill_in :song_length, with: length
+    fill_in :song_play_count, with: play_count
+
+    click_on "Create Song"
+
+    new_song = Song.last
+
+    expect(current_path).to eq(song_path(new_song))
+    expect(page).to have_content(title)
+    expect(page).to have_content(length)
+    expect(page).to have_content(play_count)
+    expect(page).to have_content(artist.name)
+  end
+end
