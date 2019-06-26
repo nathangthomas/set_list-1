b5002a55a58d264e160e9a6fd44f957c64c6ebf0
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-06-26 12:00:09 -0600

    Create Artist

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
+
+  get '/artists', to: 'artists#index'
+  get '/artists/new', to: 'artists#new'
+  post '/artists', to: 'artists#create'
 end
