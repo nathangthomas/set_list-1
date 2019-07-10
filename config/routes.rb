01f1b7cb5567594c79fedf402d8a9f4a4a6f2ee1
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-07-09 20:43:45 -0600

    Sad Path & Flash

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
 
-  get '/artists', to: 'artists#index'
-  get '/artists/new', to: 'artists#new'
-  post '/artists', to: 'artists#create'
-  delete '/artists/:id', to: 'artists#destroy'
-  get '/artists/:id/edit', to: 'artists#edit'
-  patch '/artists/:id', to: 'artists#update'
-
+  # get '/artists', to: 'artists#index'
+  # get '/artists/new', to: 'artists#new'
+  # post '/artists', to: 'artists#create'
+  # delete '/artists/:id', to: 'artists#destroy'
+  # get '/artists/:id/edit', to: 'artists#edit'
+  # patch '/artists/:id', to: 'artists#update'
+  resources :artists
   resources :playlists, only: :index
 
   #cart
